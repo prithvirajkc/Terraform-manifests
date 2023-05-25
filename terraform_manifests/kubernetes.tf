258f9c5a44f00d84e28396550f95e0244e0c4c98
prithvirajkc <prithviraj1998@hotmail.com>
2023-05-25 17:06:12 +0530

    depends_on

--- terraform_manifests/kubernetes.tf
+++ terraform_manifests/kubernetes.tf
@@ -5,4 +5,5 @@ module "Kubernetes" {
   aks_rg_name = azurerm_resource_group.rg01.name
   client_id = module.service_principal.client_id
   client_secret = module.service_principal.client_password
+  depends_on = [ module.service_principal ]
 }
