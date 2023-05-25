55fb28096825d5b99cfb6bc15a65c5f317a53432
prithvirajkc <prithviraj1998@hotmail.com>
2023-05-25 17:49:51 +0530

    deleted sp

--- terraform_manifests/kubernetes.tf
+++ terraform_manifests/kubernetes.tf
@@ -1,9 +1,16 @@
+data "azuread_service_principal" "sp01" {
+  display_name = "sp-AKS"
+}
+
+resource "azuread_service_principal_password" "pwd" {
+  service_principal_id = data.azuread_service_principal.sp01.object_id
+}
+
 module "Kubernetes" {
   source = "./modules/AKS"
   aks_location = azurerm_resource_group.rg01.location
   aks_name = var.aks_name 
   aks_rg_name = azurerm_resource_group.rg01.name
-  client_id = module.service_principal.client_id
-  client_secret = module.service_principal.client_password
-  depends_on = [ module.service_principal ]
+  client_id = data.azuread_service_principal.sp01.application_id
+  client_secret = azuread_service_principal_password.pwd.value
 }
--- terraform_manifests/modules/Service_Principal/variable.tf
+++
@@ -1,4 +0,0 @@
-variable "sp_name" {
-  type = string
-  description = "Name of the service principal"
-}
